1f47a0ffc5b91b0b9721fac72548e8665f8a734a
GabrielzinJacques <vinicius-jacques@hotmail.com>
2022-07-07 18:01:10 -0300

    Requisitos 01_

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,42 @@
+import { INTEGER, Model } from 'sequelize/types';
+import db from '.';
+
+export default class Matches extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  awayTeamGoals!: number;
+  inProgress!: number;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, { sequelize: db,
+  modelName: 'matches',
+  timestamps: false });
---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,22 @@
+import { INTEGER, Model, STRING } from 'sequelize/types';
+import db from '.';
+
+export default class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, { sequelize: db,
+  modelName: 'teams',
+  timestamps: false });
